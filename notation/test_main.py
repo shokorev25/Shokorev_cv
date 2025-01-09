eb6a472f75352146d42aa8bbfdb25401402817ee
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-01-10 00:33:55 +0800

    Update test_main.py

--- notation/test_main.py
+++ notation/test_main.py
@@ -5,13 +5,13 @@ def test_valid_expressions():
     assert prefix_to_infix("+ - 13 4 55") == "((13 - 4) + 55)"
     assert prefix_to_infix("+ 2 * 2 - 2 1") == "(2 + (2 * (2 - 1)))"
     assert prefix_to_infix("+ + 10 20 30") == "((10 + 20) + 30)"
-    assert prefix_to_infix("- - 1 2 3") == "((1 - 2) - 3)"
+    assert prefix_to_infix("- - 1 2 ") == "(1 - 2)"
     assert prefix_to_infix("/ + 3 10 * + 2 3 - 3 5") == "((3 + 10) / ((2 + 3) * (3 - 5)))"
 
 def test_invalid_expressions():
     with pytest.raises(ValueError, match="Выражение не должно быть пустым"):
         prefix_to_infix("")
-    with pytest.raises(ValueError, match="Недостаточно операндов для оператора"):
+    with pytest.raises(ValueError, match="Недостаточно операндов"):
         prefix_to_infix("+ 1")
     with pytest.raises(ValueError, match="Недопустимый символ: x"):
         prefix_to_infix("+ 1 x")
